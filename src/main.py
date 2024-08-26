788a791e501e2425ed519f7a6a7b9d9ee554bc04
MarcosReboucasDS <duartemarcosreboucas@gmail.com>
2024-08-26 20:28:30 -0300

    Continuação da aula

--- src/main.py
+++ src/main.py
@@ -24,21 +24,24 @@ if __name__ == "__main__":
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input('Quanto há no tanque? '))
+    cm = float(input('Qual o consumo medio? '))
 
-
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, motor = False)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
 
     print('Cadastre um carro')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input('Quanto há no tanque? '))
+    cm = float(input('Qual o consumo medio? '))
 
-    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, motor = False)
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
 
     '''
-    Controlando o carro até ele atingir 10000 Km
+    Controlando o carro até ele atingir 300 Km
     '''
-    while carro1.odometro < 600 and carro2.odometro < 600:
+    while carro1.odometro < 600 and carro2.odometro < 600 and (carro1.tanque > 0 and carro2.tanque > 0):
         try:
             op = 0
             while op not in (1,2):
@@ -59,4 +62,3 @@ if __name__ == "__main__":
     print(carro1)
     print(carro2)
     print('Parar para trocar óleo!!!')
-
